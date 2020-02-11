9b1d6340b3b6c5f37bc6f1c91b956b86b3bc8ce1
James Radtke <cloudxabide@gmail.com>
2020-02-10 20:52:12 -0500

    First TF commit

---
+++ Templates/Terraform/variables.tf
@@ -0,0 +1,23 @@
+# variables.tf
+variable "aws_region" {
+	default = "us-gov-west-1"
+}
+
+variable "vpc_cidr" {
+	default = "10.160.0.0/16"
+}
+
+variable "subnets_rublic_cidr" {
+	type = "list"
+	default = ["10.160.0.0/22", "10.160.4.0/22"]
+}
+
+variable "subnets_private_cidr" {
+	type = "list"
+	default = ["10.160.24.0/22", "10.160.28.0/22"]
+}
+
+variable "azs" {
+	type = "list"
+	default = ["us-gov-west-1a", "us-gov-west-1b"]
+}
